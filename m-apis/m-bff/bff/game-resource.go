e16eea54ecac80ddc5ca1e2e9b8ccbd3d7ad03fa
kshneo0 <kshneo@gmail.com>
2022-02-16 10:18:44 +0900

    sec4 ch32 Functions to get clients

--- m-apis/m-bff/bff/game-resource.go
+++ m-apis/m-bff/bff/game-resource.go
@@ -3,6 +3,8 @@ package bff
 import (
 	pbgameengine "github.com/gRPC/m-apis/m-game-engine/v1"
 	pbhighscore "github.com/gRPC/m-apis/m-highscore/v1"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
 )
 
 type gameResource struct {
@@ -15,4 +17,42 @@ func NewGameResource(gameClient pbhighscore.GameClient, gameEngineClient pbgamee
 		gameClient:       gameClient,
 		gameEngineClient: gameEngineClient,
 	}
+}
+
+func NewGrpcGameServiceClient(serverAddr string) (pbhighscore.GameClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal().Msgf("Failed to dial: %v", err)
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-highscore connection is nil in m-bff")
+	}
+
+	client := pbhighscore.NewGameClient(conn)
+
+	return client, nil
+}
+
+func NewGrpcGameEngineServiceClient(serverAddr string) (pbgameengine.GameEngineClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal().Msgf("Failed to dial: %v", err)
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-game-engine connection is nil in m-bff")
+	}
+
+	client := pbgameengine.NewGameEngineClient(conn)
+
+	return client, nil
 }
