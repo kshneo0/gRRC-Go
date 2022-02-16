ae3a357067b0b1fe3044f304b910ed5ea7d2112d
kshneo0 <kshneo@gmail.com>
2022-02-16 10:35:28 +0900

    sec4 ch34 Create handlers for m-highscore

--- m-apis/m-bff/bff/game-resource.go
+++ m-apis/m-bff/bff/game-resource.go
@@ -1,8 +1,12 @@
 package bff
 
 import (
+	"context"
+	"strconv"
+
 	pbgameengine "github.com/gRPC/m-apis/m-game-engine/v1"
 	pbhighscore "github.com/gRPC/m-apis/m-highscore/v1"
+	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
@@ -55,4 +59,30 @@ func NewGrpcGameEngineServiceClient(serverAddr string) (pbgameengine.GameEngineC
 	client := pbgameengine.NewGameEngineClient(conn)
 
 	return client, nil
+}
+
+func (gr *gameResource) SetHighScore(c *gin.Context) {
+	highScoreString := c.Param("hs")
+	highScoreFloat64, err := strconv.ParseFloat(highScoreString, 64)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to convert highscore to float")
+	}
+	gr.gameClient.SetHighScore(context.Background(), &pbhighscore.SetHighScoreRequest{
+		HighScore: highScoreFloat64,
+	})
+}
+
+func (gr *gameResource) GetHighScore(c *gin.Context) {
+
+	highScoreResponse, err := gr.gameClient.GetHighScore(context.Background(), &pbhighscore.GetHighScoreRequest{})
+	if err != nil {
+		log.Error().Err(err).Msg("Error while getting highscore")
+		return
+	}
+	hsString := strconv.FormatFloat(highScoreResponse.HighScore, 'e', -1, 64)
+
+	c.JSONP(200, gin.H{
+		"hs": hsString,
+	})
+
 }
